600f6e0a99027e132e8b1df4142a833b1da6b8b2
nirfer13 <86719868+nirfer13@users.noreply.github.com>
2023-11-19 20:41:54 +0100

    Fix time.

--- cogs/spawnboss.py
+++ cogs/spawnboss.py
@@ -123,7 +123,7 @@ class message(commands.Cog, name="spawnBoss"):
             if timestamp.strftime("%H:%M UTC") == "04:10 UTC":
                 await functions_daily.clear_daily_file(self)
 
-            if timestamp.strftime("%H:%M UTC") == "20:40 UTC" or \
+            if timestamp.strftime("%H:%M UTC") == "21:45 UTC" or \
                 timestamp.strftime("%H:%M UTC") == "5:30 UTC":
                 await functions_twitch.assign_roles_messages(self)
                 await functions_twitch.assign_roles_watchtime(self)
